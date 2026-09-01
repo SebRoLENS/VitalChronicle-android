af295575144596f69b84af413c7af2678b9db539
Sebastiano Romi <romi@lens.unifi.it>
2026-09-01 14:48:40 +0200

    Bump Android version to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.sebrolens.vitalchronicle.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
